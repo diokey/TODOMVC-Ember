24e87134df7db7143c50a300ef264ea71ad6d910
Olivier Dusabimana <diokeyolivier@gmail.com>
2015-02-28 18:19:31 -0500

    Marking todo as complete or incomplete

--- js/controllers/todo_controller.js
+++ js/controllers/todo_controller.js
@@ -16,3 +16,17 @@ Todos.TodosController = Ember.Controller.extend({
     }
   }
 });
+
+Todos.TodoController = Ember.ObjectController.extend({
+  isCompleted : function (key, value) {
+    var model = this.get('model');
+    if ( value === undefined) {
+      return model.get('isCompleted');
+    } else {
+      model.set('isCompleted', value);
+      model.save();
+
+      return value;
+    }
+  }.property('model.isCompleted')
+});
