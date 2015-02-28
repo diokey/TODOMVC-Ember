7abedd5c9110544259af60abaeef1360164e1f16
Olivier Dusabimana <diokeyolivier@gmail.com>
2015-02-28 17:42:10 -0500

    Display complete stated

--- js/models/todo.js
+++ js/models/todo.js
@@ -7,16 +7,16 @@ Todos.Todo.FIXTURES = [
   {
     id : 1,
     title : 'Learn Ember.js',
-    completed : true
+    isCompleted : true
   },
   {
     id : 2,
     title : '...',
-    completed : false
+    isCompleted : false
   },
   {
     id : 3,
     title : 'Profit!',
-    completed : false
+    isCompleted : false
   }
 ];
