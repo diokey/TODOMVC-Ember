7738c487bc88652d9f08dd376bbd05c0f4f913cc
Olivier Dusabimana <diokeyolivier@gmail.com>
2015-03-01 14:49:33 -0500

    Added active route

--- js/route.js
+++ js/route.js
@@ -2,7 +2,7 @@ Todos.Router.map(function () {
   this.resource('todos', {
     path : '/'
   }, function () {
-    
+    this.route('active');
   });
 });
 
@@ -15,5 +15,18 @@ Todos.TodosIndexRoute = Ember.Route.extend({
 Todos.TodosRoute = Ember.Route.extend({
   model : function () {
     return this.store.find('todo');
+  }
+});
+
+Todos.TodosActiveRoute = Ember.Route.extend({
+  model : function () {
+    return this.store.filter('todo', function (todo) {
+      return !todo.get('isCompleted');
+    });
+  },
+  renderTemplate : function (controller) {
+    this.render('todos/index', {
+      controller : controller
+    });
   }
 });
