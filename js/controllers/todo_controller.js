1526dc5b8b7f06ae3082dc186dfdcd43d650d730
Olivier Dusabimana <diokeyolivier@gmail.com>
2015-02-28 23:05:43 -0500

    Accept edits

--- js/controllers/todo_controller.js
+++ js/controllers/todo_controller.js
@@ -33,7 +33,22 @@ Todos.TodoController = Ember.ObjectController.extend({
     editTodo : function () {
       console.log('called');
      this.set('isEditing',true);
-    } 
+    },
+    acceptChanges : function () {
+      this.set('isEditing', false);
+
+      if(Ember.isEmpty(this.get('model.title'))) {
+        this.send('removeTodo');
+      } else {
+        this.get('model').save();
+      }
+    },
+    removeTodo : function () {
+      var todo = this.get('model');
+
+      todo.deleteRecord();
+      todo.save();
+    }
   },
 
   isEditing : false,
