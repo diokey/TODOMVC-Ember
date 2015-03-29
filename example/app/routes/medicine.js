679937d084288a4b3bfca197e873e3b923445fbf
Olivier Dusabimana <diokeyolivier@gmail.com>
2015-03-29 19:44:37 -0400

    Refractoring

--- example/app/routes/medicine.js
+++ example/app/routes/medicine.js
@@ -1,7 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  model : function () {
+  model : function (params) {
     var medicines = this.store.find('medicine');
 
     return medicines;
@@ -9,7 +9,6 @@ export default Ember.Route.extend({
   setupController : function (controller, model) {
     this._super(controller, model);
     var pagingAndSorting = this.store.find('pagingAndSorting',1);
-    console.log(pagingAndSorting);
     controller.set('pagingAndSorting', pagingAndSorting);
   }
 });
--- example/app/serializers/medicine.js
+++ example/app/serializers/medicine.js
@@ -1,16 +1,11 @@
 import DS from 'ember-data';
 
 export default DS.JSONSerializer.extend(DS.EmbeddedRecordsMixin, {
-  attrs : {
-    pagingAndSorting : {embedded : 'always'},
-    list : {embedded : 'always'}
-  },
   extractArray: function(store, type, payload) {
     var pagination = payload.pagingAndSorting;
     pagination.fullListSize = payload.fullListSize;
     pagination.id = 1;
     store.createRecord('pagingAndSorting',pagination);
-    console.log(pagination);
     return payload.list;
   }
 });
